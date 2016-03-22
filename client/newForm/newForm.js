0245a89fb2e0230c4f9a1f6bb90309db0b8d5a99
DanielDeMarco <danielanthonydemarco@gmail.com>
2016-03-21 20:23:59 -0400

    added session

--- client/newForm/newForm.js
+++ client/newForm/newForm.js
@@ -1,15 +1,17 @@
 Template.newForm.events({
 	"submit form": function(e, template) {
 		e.preventDefault();
-		let someElement = template.find("#firstName").value;
+		let firstName = template.find("#firstName").value;
 		let lastName = template.find("#lastName").value;
 		let quote = template.find("#quote").value;
 		let gender = template.find("#gender").value;
-		let link = template.find("#githubLink").value;
-		console.log(someElement);	
-		console.log(lastName);
-		console.log(quote);
-		console.log(gender);
-		console.log(link);
+		let githubLink = template.find("#githubLink").value;
+		Session.set('firstName', firstName);
+		Session.set('lastName', lastName);
+		Session.set('quote', quote);
+		Session.set('gender', gender);
+		Session.set('githubLink', githubLink);
+		Router.go('/bCard');
 	}
+
 });
